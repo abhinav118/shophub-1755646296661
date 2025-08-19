a2ddc162cac7224941e29ef71b46ca74477db3f4
Abhi <abhinav118@gmail.com>
2025-08-19 19:31:45 -0400

    Add components/UserMenu.tsx

---
+++ components/UserMenu.tsx
@@ -0,0 +1,12 @@
+interface UserMenuProps {
+  user?: any
+  menuItems?: any
+}
+
+export default function UserMenu({ user, menuItems }: UserMenuProps) {
+  return (
+    <div className="">
+      {/* UserMenu component content */}
+    </div>
+  )
+}
