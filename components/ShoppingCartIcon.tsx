3b98aff55f6e1ab2eaa81c0fbde6aa0eab59013b
Abhi <abhinav118@gmail.com>
2025-08-19 19:31:45 -0400

    Add components/ShoppingCartIcon.tsx

---
+++ components/ShoppingCartIcon.tsx
@@ -0,0 +1,12 @@
+interface ShoppingCartIconProps {
+  itemCount?: any
+  onClick?: any
+}
+
+export default function ShoppingCartIcon({ itemCount, onClick }: ShoppingCartIconProps) {
+  return (
+    <div className="">
+      {/* ShoppingCartIcon component content */}
+    </div>
+  )
+}
