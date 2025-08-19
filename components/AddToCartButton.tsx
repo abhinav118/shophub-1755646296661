fbad77ce6aaf3a5146328c965b8a520477f98f76
Abhi <abhinav118@gmail.com>
2025-08-19 19:31:44 -0400

    Add components/AddToCartButton.tsx

---
+++ components/AddToCartButton.tsx
@@ -0,0 +1,13 @@
+interface AddToCartButtonProps {
+  productId?: any
+  quantity?: any
+  variant?: any
+}
+
+export default function AddToCartButton({ productId, quantity, variant }: AddToCartButtonProps) {
+  return (
+    <div className="">
+      {/* AddToCartButton component content */}
+    </div>
+  )
+}
