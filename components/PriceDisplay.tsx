7a93f08a92d1b69f7d2db3956a11b2015bde48fb
Abhi <abhinav118@gmail.com>
2025-08-19 19:31:44 -0400

    Add components/PriceDisplay.tsx

---
+++ components/PriceDisplay.tsx
@@ -0,0 +1,13 @@
+interface PriceDisplayProps {
+  price?: any
+  originalPrice?: any
+  currency?: any
+}
+
+export default function PriceDisplay({ price, originalPrice, currency }: PriceDisplayProps) {
+  return (
+    <div className="">
+      {/* PriceDisplay component content */}
+    </div>
+  )
+}
