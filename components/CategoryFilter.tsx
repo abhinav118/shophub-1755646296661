fed4066287dd470633f53bc058b23e896e2b7b97
Abhi <abhinav118@gmail.com>
2025-08-19 19:31:45 -0400

    Add components/CategoryFilter.tsx

---
+++ components/CategoryFilter.tsx
@@ -0,0 +1,13 @@
+interface CategoryFilterProps {
+  categories?: any
+  multiple?: any
+  placeholder?: any
+}
+
+export default function CategoryFilter({ categories, multiple, placeholder }: CategoryFilterProps) {
+  return (
+    <div className="">
+      {/* CategoryFilter component content */}
+    </div>
+  )
+}
