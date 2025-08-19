1b43aa78c7845a63d7fb3095d726cae6ab49680c
Abhi <abhinav118@gmail.com>
2025-08-19 19:31:43 -0400

    Add components/ProductCard.tsx

---
+++ components/ProductCard.tsx
@@ -0,0 +1,15 @@
+interface ProductCardProps {
+  image?: any
+  title?: any
+  price?: any
+  rating?: any
+  discount?: any
+}
+
+export default function ProductCard({ image, title, price, rating, discount }: ProductCardProps) {
+  return (
+    <div className="">
+      {/* ProductCard component content */}
+    </div>
+  )
+}
